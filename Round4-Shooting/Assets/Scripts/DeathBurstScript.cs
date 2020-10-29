c315c9bc7f875706ef93fb68e4b865610003ca99
Eiichi Takebuchi <nanashi4129@gmail.com>
2020-10-29 14:07:21 +0900

    コメントを少し追加

--- Round4-Shooting/Assets/Scripts/DeathBurstScript.cs
+++ Round4-Shooting/Assets/Scripts/DeathBurstScript.cs
@@ -6,7 +6,7 @@ public class DeathBurstScript : MonoBehaviour
 {
     IEnumerator DestroyMyself()
     {
-        // 5秒後に自分自身を削除
+        // 5秒後に自分自身を削除，５秒間炎上する
         yield return new WaitForSeconds(5f);
         Destroy(this.gameObject);
     }
