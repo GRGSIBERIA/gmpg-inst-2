65221dd1f82d89c20c3d4c26365642bc2f75a85f
Eiichi Takebuchi <nanashi4129@gmail.com>
2020-11-07 15:56:13 +0900

    アニメーションを清く正しく再生させる

--- Round5-ManyExample/Assets/Scripts/DoorOpenScript.cs
+++ Round5-ManyExample/Assets/Scripts/DoorOpenScript.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Animation))]
+[RequireComponent(typeof(Animator))]
 public class DoorOpenScript : MonoBehaviour
 {
-    Animation animationComponent;
+    Animator animationComponent;
 
     /// <summary>
     /// ドアの開閉フラグ
@@ -15,7 +15,10 @@ public class DoorOpenScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        animationComponent = this.GetComponent<Animation>();
+        animationComponent = this.GetComponent<Animator>();
+
+        // 自動再生を防ぐための仕組み
+        animationComponent.enabled = false;
     }
 
     // Update is called once per frame
@@ -28,26 +31,32 @@ public class DoorOpenScript : MonoBehaviour
     /// ドアを開閉して，ドアの開閉フラグを反転する
     /// </summary>
     /// <param name="clipName">アニメーションクリップ名</param>
-    public void GetClipAsPlay(string clipName)
+    public IEnumerator GetClipAsPlay(string clipName)
     {
         // 仮引数から再生するクリップを取得して再生する
-        var clip = animationComponent.GetClip(clipName);
-        animationComponent.clip = clip;
-        animationComponent.Play();
+        animationComponent.enabled = true;
+        animationComponent.Play(clipName);
+
+        // defaultレイヤーのクリップについて再生秒数を取得する
+        var seconds = animationComponent.GetCurrentAnimatorStateInfo(0).length;
+
+        yield return new WaitForSeconds(seconds);
 
         // ドアの開閉フラグを反転
         isOpen = !isOpen;
+        animationComponent.enabled = false; // 自動再生防止装置
     }
 
     public void DoorOperation()
     {
+        // コルーチンを呼び出して指定秒数だけアニメーションを再生させる
         if (isOpen)
         {
-            GetClipAsPlay("DoorClose");
+            StartCoroutine("GetClipAsPlay", "DoorClose");
         }
         else
         {
-            GetClipAsPlay("DoorOpen");
+            StartCoroutine("GetClipAsPlay", "DoorOpen");
         }
     }
 }
