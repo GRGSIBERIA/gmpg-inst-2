25dfca5eb414dd53dc2c128d70681d28cf624713
Eiichi Takebuchi <nanashi4129@gmail.com>
2020-11-17 15:36:53 +0900

    死亡したときの挙動を書く

--- Round6-GetItem/Assets/Scripts/DeceaseMonitorScript.cs
+++ Round6-GetItem/Assets/Scripts/DeceaseMonitorScript.cs
@@ -8,6 +8,12 @@ using UnityEngine;
 public class DeceaseMonitorScript : MonoBehaviour
 {
     /// <summary>
+    /// 死亡したときのParticleエフェクト
+    /// </summary>
+    [SerializeField]
+    GameObject deceaseParticle;
+
+    /// <summary>
     /// プレイヤーの姿勢をキャッシュする
     /// </summary>
     Transform player;
@@ -16,6 +22,12 @@ public class DeceaseMonitorScript : MonoBehaviour
     void Start()
     {
         // タグからプレイヤーを呼び出す
+        // SerializeFieldを使えばInspectorに編集できるが
+        // 頻繁に更新したり，中身が変わるようなものはTagを使って直接呼び出すほうがいい
+        // NOTE:
+        // Windowsの再起動，ブルースクリーン等でシーンが破壊されると
+        // Prefabの内容が書き変わっていてInspectorにプレイヤーを延々とアタッチし直すことがある
+        // この作業はとてつもなく面倒でバグを生みやすいので，Tagを使う場合が十分にあり得る
         player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
@@ -25,8 +37,13 @@ public class DeceaseMonitorScript : MonoBehaviour
         // プレイヤーの一定以上の落下を検知したらプレイヤーを死亡させる
         if (player.position.y < -50f)
         {
+            // カメラをプレイヤーと一緒に削除しないようにHierarchyのトップに移動する
+            Camera.main.transform.parent = null;
+
             // プレイヤーを逝去させる
-            Destroy(player);
+            // プレイヤーの位置にパーティクルを発生させる
+            Instantiate(deceaseParticle, player.position, player.rotation);
+            Destroy(player.gameObject);
         }
     }
 }
