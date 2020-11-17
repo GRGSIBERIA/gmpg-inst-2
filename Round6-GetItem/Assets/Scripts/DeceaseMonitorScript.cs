106824bde171ae82e44b356748c6210a2bc00851
Eiichi Takebuchi <nanashi4129@gmail.com>
2020-11-17 13:26:24 +0900

    死亡を監視するスクリプトを書く

--- Round6-GetItem/Assets/Scripts/DeceaseMonitorScript.cs
+++ Round6-GetItem/Assets/Scripts/DeceaseMonitorScript.cs
@@ -2,17 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// プレイヤーの死亡をいちいち監視するためのスクリプト
+/// </summary>
 public class DeceaseMonitorScript : MonoBehaviour
 {
+    Transform player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // プレイヤーの一定以上の落下を検知したらプレイヤーを死亡させる
+        if (player.position.y < -50f)
+        {
+            // プレイヤーを逝去させる
+            Destroy(player);
+        }
     }
 }
