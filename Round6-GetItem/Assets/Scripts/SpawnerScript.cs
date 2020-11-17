08ac9f8671bb59aeec2a9552ae2abf24be65405a
Eiichi Takebuchi <nanashi4129@gmail.com>
2020-11-17 13:11:59 +0900

    足場を生成するスクリプトを作る

---
+++ Round6-GetItem/Assets/Scripts/SpawnerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 足場を生成するためのスクリプト
+/// </summary>
+public class SpawnerScript : MonoBehaviour
+{
+    /// <summary>
+    /// 足場の基本となるプレファブ
+    /// </summary>
+    [SerializeField]
+    GameObject baseStep;
+
+    GameObject[] steps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // ゲームスタート時に最初の足場を生成する
+        GameObject firstStep = Instantiate(
+            baseStep, 
+            new Vector3(0f, -1.5f, 0f), 
+            new Quaternion(0f, 0f, 0f, 0f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
