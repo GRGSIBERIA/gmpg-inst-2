3d14438dbcbe893f5522083059ad2594d3a47d2d
Eiichi Takebuchi <nanashi4129@gmail.com>
2020-11-17 15:36:41 +0900

    難易度構造体を追加する

--- Round6-GetItem/Assets/Scripts/SpawnerScript.cs
+++ Round6-GetItem/Assets/Scripts/SpawnerScript.cs
@@ -3,6 +3,66 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
+/// 難易度を格納するためのクラス
+/// 配列の中にクラスを入れてInspectorに表示するには
+/// System.Serializableをクラス宣言の前に書いておく
+/// </summary>
+[System.Serializable]
+public class DifficultyLevel
+{
+    /// <summary>
+    /// 難易度の名前
+    /// </summary>
+    [SerializeField]
+    string _name;
+
+    /// <summary>
+    /// プレイヤーとの距離
+    /// </summary>
+    [SerializeField]
+    float _distance;
+    
+    /// <summary>
+    /// 加点スコア
+    /// </summary>
+    [SerializeField]
+    int _point;
+
+    /// <summary>
+    /// ステップの色
+    /// </summary>
+    [SerializeField]
+    Material _colorMaterial;
+
+    //******************************************************
+    /// プロパティの宣言, プロパティとは取得(get)，設定(set)用の関数の別名
+    /// _name, _distance, _pointは誰かに書き換えて欲しくない
+    /// しかし，それぞれ取得だけはできて欲しい場合にgetアクセサを使ってプロパティ宣言をすると
+    /// 取得はできるが外部から変更が不可能なプログラムが書ける
+    /// 外部からの変更を禁止することで，バグの発生個所を狭めることができる
+    public string Name { get { return _name; }}
+
+    public float Distance { get { return _distance; }}
+
+    public int Point { get { return _point; }}
+
+    public Material ColorMaterial { get { return _colorMaterial; }}
+    //******************************************************
+
+    /// <summary>
+    /// クラス名の関数はコンストラクタとして扱われる
+    /// </summary>
+    /// <param name="name">難易度の名前</param>
+    /// <param name="distance">スポーンされる距離</param>
+    /// <param name="point">得点</param>
+    /// <param name="material">色</param>
+    public DifficultyLevel(string name, float distance, int point, Material material)
+    {
+        
+    }
+}
+
+/// <summary>
 /// 足場を生成するためのスクリプト
 /// </summary>
 public class SpawnerScript : MonoBehaviour
@@ -12,6 +72,12 @@ public class SpawnerScript : MonoBehaviour
     /// </summary>
     [SerializeField]
     GameObject baseStep;
+
+    /// <summary>
+    /// 難易度の配列
+    /// </summary>
+    [SerializeField]
+    DifficultyLevel[] levels;
 
     /// <summary>
     /// 得点に対して増えていく度合
@@ -35,6 +101,17 @@ public class SpawnerScript : MonoBehaviour
 
         // PlayerのTransformをキャッシュしておく
         player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        // 誰も何も設定していなかった事故が発生！
+        if (levels.Length <= 0)
+        {
+            levels = new DifficultyLevel[] {  // 配列はnewで初期化しなければならない
+                new DifficultyLevel(    // new でコンストラクタ呼び出し
+                    "Normal", 10f, 1,   // 距離と得点の順番
+                    new Material(Shader.Find("Standard")) // 標準シェーダを探してマテリアルに貼り付ける
+                )
+            };  // ネスト(入れ子)が深くなる初期化はこのようにインデントを使って読みやすく工夫する
+        }
     }
 
     // Update is called once per frame
@@ -49,6 +126,6 @@ public class SpawnerScript : MonoBehaviour
     /// </summary>
     void Spawn()
     {
-
+        
     }
 }
