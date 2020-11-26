057dfffe96b5f1ca50d3b83538bccdbd954f7b3d
Eiichi Takebuchi <nanashi4129@gmail.com>
2020-11-26 13:57:59 +0900

    SE関連の調整

---
+++ Round6-GetItem/Assets/Scripts/TouchSoundEffectManagerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchSoundEffectManagerScript : MonoBehaviour
+{
+    /// <summary>
+    /// 足場を踏んだ時に鳴らす効果音の配列
+    /// </summary>
+    [SerializeField]
+    List<AudioClip> clips = new List<AudioClip>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TouchScaffold()
+    {
+        int index = Random.Range(0, clips.Count);
+    }
+}
