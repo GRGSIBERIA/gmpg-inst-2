8f19bbe55afa30da7f9fb096efd24a960cd6da63
Eiichi Takebuchi <nanashi4129@gmail.com>
2020-11-26 15:58:49 +0900

    ぬるぬる動かない

--- Round6-GetItem/Assets/Scripts/CameraDrivenScript.cs
+++ Round6-GetItem/Assets/Scripts/CameraDrivenScript.cs
@@ -11,14 +11,9 @@ public class CameraDrivenScript : MonoBehaviour
     float power = 1f;
 
     /// <summary>
-    /// プレイヤーのキャッシュ
+    /// プレイヤーのRigidbodyのキャッシュ
     /// </summary>
-    Transform player;
-
-    /// <summary>
-    /// プレイヤーの過去の時間
-    /// </summary>
-    Vector3 prevPosition;
+    Rigidbody playerrb;
 
     /// <summary>
     /// 過去の速度
@@ -34,37 +29,24 @@ public class CameraDrivenScript : MonoBehaviour
     void Start()
     {
         // プレイヤーのキャッシュを確保する
-        player = GameObject.FindWithTag("Player").transform;
-
-        // 初期値として現在のプレイヤーの位置を代入しておく
-        prevPosition = player.position;
+        playerrb = GameObject.FindWithTag("Player").GetComponent<Rigidbody>();
 
         // Transformをキャッシュする
         ts = transform;
+
+        // 過去の速度の初期値は現在速度
+        prevVelocity = playerrb.velocity;
     }
 
     // Update is called once per frame
     void Update()
     {
         // ゲームセット時にはPlayerが破棄されるので動くのを止める
-        if (player == null) return;
+        if (playerrb == null) return;
 
-        // プレイヤーの位置を時間で微分して速度を得る
-        Vector3 velocity = (player.position - prevPosition) * Time.deltaTime;
-
-        // さらに速度を微分して加速度を得る
-        Vector3 accel = (velocity - prevVelocity) * Time.deltaTime;
+        Vector3 velocity = playerrb.velocity * Time.deltaTime;
+        velocity.z = -10f;
         
-        // カメラの動きは加速度でコントロールする
-        Vector3 cameraPosition = accel * power;
-        cameraPosition.z = ts.localPosition.z;  // Zの位置だけは固定
-
-        // あえてカメラをプレイヤー速度に代入することで，カメラの振りを実現する
-        // localPositionはローカル座標系の位置
-        ts.localPosition = cameraPosition;
-
-        // 現在と過去を置き換える
-        prevPosition = player.position;
-        prevVelocity = velocity;
+        ts.localPosition = velocity;
     }
 }
